9bc2874b899754a27d63b1a0be464e9eb9b4c20e
jakoblrosen <jake3.rosen@gmail.com>
2021-11-13 15:58:50 -0700

    removed redundant comment

--- code_1/hash.cpp
+++ code_1/hash.cpp
@@ -157,19 +157,7 @@ bool HashTable::insertItem(string key, int cNum)
     }
 }
 
-// function to display hash table //
-/* assume the table size is 5. For each bucket it will show the
-** the string key and the commit number (separated by comma) within parenthesis
-** e.g. the key is science and commit numbers are 1 and 4. The key science
-** is hashed to position 0. So the print format will be-
-
-0|| science(1,4,)
-1||
-2||
-3||
-4|| difficult(3,)-->fun(2,)-->computer(0,)
-
-*/
+// function to display hash table 
 void HashTable::printTable()
 {
     HashNode *currNode = nullptr;
