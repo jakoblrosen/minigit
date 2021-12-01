af1e81dfbca156487071b8c2dd4d483129a00ae2
jakoblrosen <jake3.rosen@gmail.com>
2021-12-01 00:23:51 -0700

    fixed test helper functions to only use CaptureStdout directly before print statements

--- tests/test_helpers.cpp
+++ tests/test_helpers.cpp
@@ -1,8 +1,6 @@
 #include <gtest/gtest.h>
 #include "test_helpers.hpp"
 
-#include "../code_1/hash.hpp"
-
 string exec(const char *cmd)
 {
     array<char, 128> buffer;
@@ -37,35 +35,78 @@ string readFileIntoString(string file_name)
 
 string test_hash(string key)
 {
-    HashTable *table = new HashTable(0);
+    HashTable table(0);
 
-    return table->hashFunction(key);
+    return table.hashFunction(key);
 }
 
 string test_insert(string arr[], int length, int table_size)
 {
-    testing::internal::CaptureStdout();
-    HashTable *table = new HashTable(table_size);
+    HashTable table(table_size);
 
     for (int i = 0; i < length; i++)
     {
-        table->insertItem(arr[i], i);
+        table.insertItem(arr[i], i);
     }
 
-    table->printTable();
+    testing::internal::CaptureStdout();
+    table.printTable();
     string output = testing::internal::GetCapturedStdout();
     return output;
 }
 
 HashNode test_search(string arr[], int length, int table_size, string search_key)
 {
-    HashTable *table = new HashTable(table_size);
+    HashTable table(table_size);
 
     for (int i = 0; i < length; i++)
     {
-        table->insertItem(arr[i], i);
+        table.insertItem(arr[i], i);
     }
 
-    HashNode *node = table->searchItem(search_key);
+    HashNode *node = table.searchItem(search_key);
     return *node;
+}
+
+string test_add(string arr[], int length, int table_size, string commit_message)
+{
+    MiniGit minigit;
+
+    minigit.init(table_size);
+
+    for (int i = 0; i < length; i++)
+    {
+        minigit.add(arr[i]);
+    }
+
+    minigit.commit(commit_message);
+
+    testing::internal::CaptureStdout();
+    minigit.printSearchTable();
+    string output = testing::internal::GetCapturedStdout();
+    return output;
+}
+
+string test_rm(string add[], int add_length, string rm[], int rm_length, int table_size, string commit_message)
+{
+    MiniGit minigit;
+
+    minigit.init(table_size);
+
+    for (int i = 0; i < add_length; i++)
+    {
+        minigit.add(add[i]);
+    }
+
+    for (int i = 0; i < rm_length; i++)
+    {
+        minigit.rm(rm[i]);
+    }
+
+    minigit.commit(commit_message);
+
+    testing::internal::CaptureStdout();
+    minigit.printSearchTable();
+    string output = testing::internal::GetCapturedStdout();
+    return output;
 }
